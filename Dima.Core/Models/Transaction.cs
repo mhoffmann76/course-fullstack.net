62d8a0df6c1b1735b33eebefa6d738324f06c823
Marcelo Hoffmann <hoffmannmarcelo@hotmail.com>
2024-07-12 10:12:55 -0300

    initial

---
+++ Dima.Core/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Models
+{
+    internal class Transaction
+    {
+        public long Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? PaidOrReceiveAt { get; set;}
+        public int type { get; set; }
+
+    }
+}
